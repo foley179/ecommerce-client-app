41093c7928aab3812c45688f92179f83268728e4
foley179 <81254761+foley179@users.noreply.github.com>
2021-09-11 18:45:23 +0100

    add signup tab and onClick to change it

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,24 +1,59 @@
+import {useState} from 'react'
+
 export default function Login(props) {
     const {changeLoginTab} = props
+    const [tabType, setTabType] = useState("login")
     function doNothing(e) {
         // to be removed once "login onclick" fixed
         e.preventDefault()
     }
-    return (
-        <div id="overlay">
-            <div id="login">
-                <h2>Login Tab</h2>
-                <form>
-                    <input type="text" placeholder="Enter Your Email" className="txt" />
-                    <input type="text" placeholder="Enter Your Password" className="txt" />
-                    {/* change onclick for login&signup button */}
-                    <button onClick={(e) => doNothing(e)} className="loginbutton button">Login</button>
-                    <button onClick={(e) => doNothing(e)} className="loginbutton button">Sign Up</button>
-                    <button onClick={(e) => changeLoginTab(e)} className="loginbutton button close">Close</button>
-                </form>
-            </div>
-        </div>
-    )
-}
 
-// change login for sign up tab onclick?
+    function handleClose(e) {
+        setTabType("login")
+        changeLoginTab(e)
+    }
+
+    function changeTabType(e) {
+        e.preventDefault()
+        if (tabType === "login") {
+            setTabType("signup")
+        } else {
+            setTabType("login")
+        }
+    }
+
+    if (tabType === "login") {
+        return (
+            <div id="overlay">
+                <div id="login">
+                    <h2>Login Tab</h2>
+                    <form>
+                        <input type="email" placeholder="Enter Your Email" className="txt" />
+                        <input type="password" placeholder="Enter Your Password" className="txt" />
+                        {/* change onclick for login button */}
+                        <button onClick={(e) => doNothing(e)} className="loginbutton button">Login</button>
+                        <button onClick={(e) => changeTabType(e)} className="loginbutton button">Sign Up</button>
+                        <button onClick={(e) => handleClose(e)} className="loginbutton button close">Close</button>
+                    </form>
+                </div>
+            </div>
+        )
+    } else {
+        return (
+            <div id="overlay">
+                <div id="login">
+                    <h2>Login Tab</h2>
+                    <form>
+                        <input type="email" placeholder="Enter Your Email" className="txt" />
+                        <input type="password" placeholder="Enter Your Password" className="txt" />
+                        <input type="password" placeholder="Re-Enter Your Password" className="txt" />
+                        {/* change onclick for signup button */}
+                        <button onClick={(e) => doNothing(e)} className="loginbutton button">Sign Up</button>
+                        <button onClick={(e) => changeTabType(e)} className="loginbutton button">Login</button>
+                        <button onClick={(e) => handleClose(e)} className="loginbutton button close">Close</button>
+                    </form>
+                </div>
+            </div>
+        )
+    }
+}
