e8d97a39115874bf657990d80cb60516b4dadf48
foley179 <81254761+foley179@users.noreply.github.com>
2021-10-17 13:46:46 +0100

    f

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname+'/client/build/index.html'));
 });
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 4000;
 app.listen(port);
 
 console.log(`Password generator listening on ${port}`);
