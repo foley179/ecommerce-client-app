849826bb175d6975530aef19cf72cae1b949b570
foley179 <81254761+foley179@users.noreply.github.com>
2021-10-18 17:38:10 +0100

    changed psql for dev/prod

--- psql.js
+++ psql.js
@@ -1,25 +1,21 @@
 const {Client} = require("pg")
 
 async function psqlQuery(query, params=[]) {
-/*    let psql
-    if (process.env.NODE_ENV === "production") {
-        psql = new Client({
-            connectionString: process.env.DATABASE_URL,
-            ssl: true
-        })
-    } else {
-        psql = new Client({
-            connectionString: `postgres://${process.env.REACT_APP_PSQL_USER}:${process.env.REACT_APP_PSQL_PASSWORD}@localhost:5432/pern_ecommerce_app`,
-        })
-    }
+/*
+    // testing (constant issues when attempting to check node_env to configure this, stuck with commenting it out for now)
+    require('dotenv').config()
+    const psql = new Client({
+        connectionString: `postgres://${process.env.REACT_APP_PSQL_USER}:${process.env.REACT_APP_PSQL_PASSWORD}@localhost:5432/pern_ecommerce_app`,
+    })
 */
-
+    // production
     const psql = new Client({
         connectionString: process.env.DATABASE_URL,
         ssl: {
             rejectUnauthorized: false
         }
     })
+
     psql.connect()
     
     let output
